d2e06a66bfe88346b582d0c78d05698ed7003e14
taitMdaka <ttmdaka@gmail.com>
2017-10-16 22:44:15 +0200

    first commit

---
+++ DataAccessLayer/Mathmethods.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class mathmatho
+    {
+        public int square(int x)
+        {
+            return x * x;
+        }
+
+        internal static void Square()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Square(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ WindowsFormsApplication1/cala.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAccessLayer
+{
+    public partial class cala : Form
+    {
+        public cala()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            mathmatho mathmatho = new mathmatho();
+            textBox2.Text = mathmatho.square(x).ToString();
+
+            //  ma = new mathmatho();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
